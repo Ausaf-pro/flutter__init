eb48c487bf2e2410faa24506abebb4e2d4c31db7
Ausaf-Pro <ausafhussain010@gmail.com>
2021-06-29 13:01:26 +0530

    day4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'pages/home_page.dart';
 import 'pages/login_page.dart';
 
@@ -11,12 +12,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         themeMode: ThemeMode.light,
-        theme: ThemeData(primarySwatch: Colors.orange),
+        theme: ThemeData(
+          primarySwatch: Colors.orange,
+          fontFamily: GoogleFonts.lato().fontFamily,
+        ),
         darkTheme: ThemeData(brightness: Brightness.dark),
-        initialRoute: '/home',
+        initialRoute: '/',
         routes: {
           "/": (context) => LoginPage(),
-          "/home" : (context) => HomePage(),
+          "/home": (context) => HomePage(),
           '/login': (context) => LoginPage(),
         });
   }
